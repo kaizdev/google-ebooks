fc51a66f4b101360c81e5859ecbe175b4b8ae615
Kai Zhang <kai.zhang@outlook.com.au>
2023-07-10 16:57:46 +1000

    adds updates to phone styling

--- src/services/ebooks-services.js
+++ src/services/ebooks-services.js
@@ -34,7 +34,7 @@ export const getEbooksBySearch = async (searchString) => {
                     thumbnail:
                         "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/128px-No-Image-Placeholder.svg.png",
                 },
-                publishedDate,
+                publishedDate = "No publish date available",
                 pageCount,
                 averageRating,
             },
